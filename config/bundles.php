b325d40acee29ab1c832a37137fd42ff30112a84
tsukiro <bastian@rasen.cl>
2018-02-27 14:51:30 -0300

    changed composer

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
